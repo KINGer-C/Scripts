72d6f9927f0baa2cd9b0dc8b78fa777f7f60c6e7
KINGer-C <paulodtna12@gmail.com>
2019-10-17 10:05:02 -0300

    new

--- up.sh
+++ up.sh
@@ -1,22 +1,21 @@
 echo "Stopping dapscoind..."
 cd /usr/local/bin && dapscoin-cli stop
 cd /usr/local/bin 
-rm -rf master_linux-v1.0.2.tar
-rm -rf master_linux-v1.0.1.3.zip
-rm -rf dapscoin-cli
-rm -rf dapscoind
-rm -rf dapscoin-qt
-rm -rf dapscoin dapscoin-poa-minerd
-rm -rf dapscoin-tx
+cd /usr/local/bin && rm -rf master_linux-v1.0.2.tar
+cd /usr/local/bin && rm -rf master_linux-v1.0.1.3.zip
+cd /usr/local/bin && rm -rf dapscoin-cli
+cd /usr/local/bin && rm -rf dapscoind
+cd /usr/local/bin && rm -rf dapscoin-qt
+cd /usr/local/bin && rm -rf dapscoin dapscoin-poa-minerd
+cd /usr/local/bin && rm -rf dapscoin-tx
 echo "Downloading update..."
-wget https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.2/master_linux-v1.0.2.tar
+cd /usr/local/bin && wget https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.2/master_linux-v1.0.2.tar
 echo "Extracting update..."
-sudo tar -xvf master_linux-v1.0.2.tar -d /usr/local/bin
-rm -rf master_linux-v1.0.2.tar
+cd /usr/local/bin && sudo tar -xvf master_linux-v1.0.2.tar
+cd /usr/local/bin && rm -rf master_linux-v1.0.2.tar
 echo "Set permissions on files..."
-sudo chmod +x /usr/local/bin/daps*
+cd /usr/local/bin && sudo chmod +x /usr/local/bin/daps*
 echo "Running dapscoind..."
-./dapscoind -daemon
+cd /usr/local/bin && ./dapscoind -daemon
 echo "Update completed!"
-cd /usr/local/bin 
-./dapscoin-cli getinfo
+cd /usr/local/bin && ./dapscoin-cli getinfo
