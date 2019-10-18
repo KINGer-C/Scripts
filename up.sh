4e5f112b09fd74813ef0dcdfba54d99c1793fc32
KINGer-C <paulodtna12@gmail.com>
2019-10-18 01:12:42 -0300

    Update up.sh

--- up.sh
+++ up.sh
@@ -13,19 +13,19 @@ cd /usr/local/bin && rm -rf dapscoin-qt
 cd /usr/local/bin && rm -rf dapscoin dapscoin-poa-minerd
 cd /usr/local/bin && rm -rf dapscoin-tx
 echo "Deleting blocks"
-cd /root/.dapscoin && rm -rf blocks 
-cd /root/.dapscoin && rm -rf chainstate 
-cd /root/.dapscoin && rm -rf database 
-cd /root/.dapscoin && rm -rf peers.dat
-cd /root/.dapscoin && rm -rf backups
-cd /root/.dapscoin && rm -rf .lock
-cd /root/.dapscoin && rm -rf banlist.dat
-cd /root/.dapscoin && rm -rf budget.dat
-cd /root/.dapscoin && rm -rf db.log
-cd /root/.dapscoin && rm -rf debug.log
-cd /root/.dapscoin && rm -rf fee_estimates.dat
-cd /root/.dapscoin && rm -rf mncache.dat
-cd /root/.dapscoin && rm -rf mnpayments.dat
+cd ~/.dapscoin && rm -rf blocks 
+cd ~/.dapscoin && rm -rf chainstate 
+cd ~/.dapscoin && rm -rf database 
+cd ~/.dapscoin && rm -rf peers.dat
+cd ~/.dapscoin && rm -rf backups
+cd ~/.dapscoin && rm -rf .lock
+cd ~/.dapscoin && rm -rf banlist.dat
+cd ~/.dapscoin && rm -rf budget.dat
+cd ~/.dapscoin && rm -rf db.log
+cd ~/.dapscoin && rm -rf debug.log
+cd ~/.dapscoin && rm -rf fee_estimates.dat
+cd ~/.dapscoin && rm -rf mncache.dat
+cd ~/.dapscoin && rm -rf mnpayments.dat
 echo "Done!"
 echo "Downloading update..."
 cd /usr/local/bin && wget https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.2/master_linux-v1.0.2.tar
