2273c8e7f9a690252371d738cc53169338f695c2
KINGer-C <paulodtna12@gmail.com>
2019-10-18 02:09:23 -0300

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,7 +1,6 @@
 echo "Stopping dapscoind..."
 cd /usr/local/bin && dapscoin-cli stop
 sleep 2s 
-cd /usr/local/bin 
 echo "Removing old files..."
 cd /usr/local/bin && rm -rf master_linux-v1.0.2.tar
 cd /usr/local/bin && rm -rf master_linux-v1.0.1.3.zip
@@ -12,6 +11,19 @@ cd /usr/local/bin && rm -rf dapscoind
 cd /usr/local/bin && rm -rf dapscoin-qt
 cd /usr/local/bin && rm -rf dapscoin dapscoin-poa-minerd
 cd /usr/local/bin && rm -rf dapscoin-tx
+echo "Done!"
+echo "Downloading update..."
+cd /usr/local/bin && wget https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.2/master_linux-v1.0.2.tar
+echo "Done!"
+echo "Extracting update..."
+cd /usr/local/bin && sudo tar -xvf master_linux-v1.0.2.tar
+echo "Done!"
+echo "Removing old files..."
+cd /usr/local/bin && rm -rf master_linux-v1.0.2.tar
+echo "Done!"
+echo "Set permissions on files..."
+cd /usr/local/bin && sudo chmod 777 daps*
+sleep 2s 
 echo "Deleting blocks"
 cd ~/.dapscoin && rm -rf blocks 
 cd ~/.dapscoin && rm -rf chainstate 
@@ -26,19 +38,6 @@ cd ~/.dapscoin && rm -rf debug.log
 cd ~/.dapscoin && rm -rf fee_estimates.dat
 cd ~/.dapscoin && rm -rf mncache.dat
 cd ~/.dapscoin && rm -rf mnpayments.dat
-echo "Done!"
-echo "Downloading update..."
-cd /usr/local/bin && wget https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.2/master_linux-v1.0.2.tar
-echo "Done!"
-echo "Extracting update..."
-cd /usr/local/bin && sudo tar -xvf master_linux-v1.0.2.tar
-echo "Done!"
-echo "Removing old files..."
-cd /usr/local/bin && rm -rf master_linux-v1.0.2.tar
-echo "Done!"
-echo "Set permissions on files..."
-cd /usr/local/bin && sudo chmod 777 /usr/local/bin/daps*
-sleep 2s 
 echo "downloading new blocks"
 cd /root/.dapscoin && wget https://github.com/DAPSCoin/BootStrap/releases/download/latest/bootstrap.zip
 unzip bootstrap.zip -d ~/.dapscoin
