c1bc343f5301d543986c5e6b6cced915cd01aaf3
KINGer-C <paulodtna12@gmail.com>
2019-10-18 00:06:57 -0300

    Update boostrap.sh

--- boostrap.sh
+++ boostrap.sh
@@ -3,10 +3,12 @@ cd /usr/local/bin && dapscoin-cli stop
 sleep 2s 
 cd /root/.dapscoin
 echo "Deleting blocks"
-rm -rf blocks
+ rm -rf ~/.dapscoin/blocks ~/.dapscoin/chainstate ~/.dapscoin/database ~/.dapscoin/peers.dat
 echo "downloading new blocks"
+cd /root/.dapscoin
 wget https://github.com/DAPSCoin/BootStrap/releases/download/latest/bootstrap.zip
 sudo unzip -jo bootstrap.zip 
+cd /root/.dapscoin
 rm -rf bootstrap.zip 
 echo "done!"
 cd /usr/local/bin && dapscoind -daemon
