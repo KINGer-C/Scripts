34781228f0e62183de75f8eb6b586d03b3ae6023
KINGer-C <paulodtna12@gmail.com>
2019-10-20 00:12:23 -0300

    mais um

--- daps.sh
+++ daps.sh
@@ -2,6 +2,7 @@ clear
 echo "Starting DAPS Masternode download and install..."
 echo "Updating/Upgrading OS..."
 sudo apt update && sudo apt upgrade -y
+echo "Swaping memory"
 sudo fallocate -l 8G /swapfile && chmod 600 /swapfile && mkswap /swapfile && swapon /swapfile && echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab && free -h
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install fail2ban -y
@@ -22,19 +23,11 @@ echo "Set permissions on files..."
 cd /usr/local/bin && sudo chmod 777 daps*
 sleep 2s 
 echo "Creating .dapscoin directory..."
+rm -rf ~/.dapscoin
+sleep 5s
 mkdir ~/.dapscoin
 cd ~/.dapscoin
 vi dapscoin.conf
 cd /usr/local/bin 
-echo "Stopping dapscoind..."
-dapscoin-cli stop
-echo "Downloading bootstrap..."
-wget https://github.com/DAPSCoin/BootStrap/releases/download/latest/bootstrap.zip
-echo "Removing old blocks, chainstate, and database folders...."
-rm -rf ~/.dapscoin/blocks ~/.dapscoin/chainstate ~/.dapscoin/database
-echo "Installing new blocks folders..."
-unzip bootstrap.zip -d ~/.dapscoin
-echo "Bootstrap installed!"
 ./dapscoind -daemon
-echo "Adding nodes"
 watch ./dapscoin-cli getinfo
