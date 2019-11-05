ad368ee9f49f7be404782db28dc6f1c35930674c
KINGer-C <paulodtna12@gmail.com>
2019-11-05 18:38:35 -0300

    Delete bootstrap.sh

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-echo "Stopping dapscoind..."
-dapscoin-cli stop
-echo "Downloading bootstrap..."
-wget https://github.com/DAPSCoin/BootStrap/releases/download/latest/bootstrap.zip
-echo "Removing old blocks, chainstate, database, and peers.dat..."
-rm -rf ~/.dapscoin/blocks ~/.dapscoin/chainstate ~/.dapscoin/database ~/.dapscoin/peers.dat
-echo "Installing new blocks folder and peers.dat..."
-unzip bootstrap.zip -d ~/.dapscoin
-echo "Bootstrap installed!"
