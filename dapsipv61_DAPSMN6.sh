cf761d0d22e66f95c04f64010d1912a0330bf229
KINGer-C <paulodtna12@gmail.com>
2019-11-06 22:19:12 -0300

    Create dapsipv61_DAPSMN6.sh

---
+++ dapsipv61_DAPSMN6.sh
@@ -0,0 +1,102 @@
+echo "########################################"
+echo "### DAPS IVP6_6 DAPSMN05 By KINGer-C ###"
+echo "########################################"
+
+echo "░░░░░▄▄▀▀▀▀▀▀▀▀▀▄▄░░░░░"
+echo "░░░░█░░░░░░░░░░░░░█░░░░"
+echo "░░░█░░░░░░░░░░▄▄▄░░█░░░"
+echo "░░░█░░▄▄▄░░▄░░███░░█░░░"
+echo "░░░▄█░▄░░░▀▀▀░░░▄░█▄░░░"
+echo "░░░█░░▀█▀█▀█▀█▀█▀░░█░░░"
+echo "░░░▄██▄▄▀▀▀▀▀▀▀▄▄██▄░░░"
+echo "░▄█░█▀▀█▀▀▀█▀▀▀█▀▀█░█▄░"
+echo "▄▀░▄▄▀▄▄▀▀▀▄▀▀▀▄▄▀▄▄░▀▄"
+echo "█░░░░▀▄░█▄░░░▄█░▄▀░░░░█"
+echo "░▀▄▄░█░░█▄▄▄▄▄█░░█░▄▄▀░"
+echo "░░░▀██▄▄███████▄▄██▀░░░"
+echo "░░░████████▀████████░░░"
+echo "░░▄▄█▀▀▀▀█░░░█▀▀▀▀█▄▄░░"
+echo "░░▀▄▄▄▄▄▀▀░░░▀▀▄▄▄▄▄▀░"
+
+sleep 3s
+echo "#######################################"
+echo "  ATENTION!!! ATENTION!!! ATENTION!!!"
+echo "#######################################"
+echo " YOU MUST HAD BEEN INSTALED"
+echo "THE MN IPV4 BEFORE RUNNING THIS SCRYPT"
+echo "#######################################"
+sleep 3s
+echo "Creating copys of executables"
+sudo cp /usr/local/bin/dapscoind /usr/local/bin/dapscoind06
+sudo cp /usr/local/bin/dapscoin-cli /usr/local/bin/dapscoin-cli06
+sudo chmod +x /usr/local/bin/daps*
+
+echo "Enter your IPV6 for the DAPSMN06"
+read IP
+echo "Enter your masternode private key for node DAPSMN06"
+read PRIVKEY
+
+CONF_DIR=~/.dapscoin06/
+CONF_FILE=dapscoin.conf
+PORT=53578
+
+mkdir -p $CONF_DIR
+echo "rpcuser=DAPSMN06" >> $CONF_DIR/$CONF_FILE
+echo "rpcpassword=DAPSMN060" >> $CONF_DIR/$CONF_FILE
+echo "rpcport=53579" >> $CONF_DIR/$CONF_FILE
+echo "rpcallowip=127.0.0.1" >> $CONF_DIR/$CONF_FILE
+echo "server=1" >> $CONF_DIR/$CONF_FILE
+echo "listen=0" >> $CONF_DIR/$CONF_FILE
+echo "daemon=1" >> $CONF_DIR/$CONF_FILE
+echo "logtimestamps=1" >> $CONF_DIR/$CONF_FILE
+echo "maxconnections=64" >> $CONF_DIR/$CONF_FILE
+echo "masternode=1" >> $CONF_DIR/$CONF_FILE
+echo "externalip=[$IP]" >> $CONF_DIR/$CONF_FILE
+echo "bind=[$IP]:53572" >> $CONF_DIR/$CONF_FILE
+echo "masternodeaddr=[$IP]:53572" >> $CONF_DIR/$CONF_FILE
+echo "masternodeprivkey=$PRIVKEY" >> $CONF_DIR/$CONF_FILE
+echo "datadir=/root/.dapscoin06" >> $CONF_DIR/$CONF_FILE
+echo "wallet=wallet06.dat" >> $CONF_DIR/$CONF_FILE
+echo "addnode=80.79.194.69:53572" >> $CONF_DIR/$CONF_FILE
+echo "addnode=212.237.7.86:53572" >> $CONF_DIR/$CONF_FILE
+echo "addnode=95.216.152.41:53572" >> $CONF_DIR/$CONF_FILE
+echo "addnode=144.202.20.83:53572" >> $CONF_DIR/$CONF_FILE
+echo "addnode=51.158.190.147:53572" >> $CONF_DIR/$CONF_FILE
+
+sudo apt-get install fail2ban -y
+sudo ufw allow ssh
+sudo ufw allow 53579
+sudo ufw enable
+cd ~
+rm -rf dapsipv61_DAPSMN06.sh
+sleep 2s
+dapscoind06 -datadir=/root/.dapscoin06 -wallet=wallet06.dat
+echo " FINISH :)"
+echo "####### DAPS - PRIVACITY MATERS #######"
+echo "███████▓█████▓▓╬╬╬╬╬╬╬╬▓███▓╬╬╬╬╬╬╬▓╬╬▓█"
+echo "████▓▓▓▓╬╬▓█████╬╬╬╬╬╬███▓╬╬╬╬╬╬╬╬╬╬╬╬╬█"
+echo "███▓▓▓▓╬╬╬╬╬╬▓██╬╬╬╬╬╬▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█" 
+echo "████▓▓▓╬╬╬╬╬╬╬▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█" 
+echo "███▓█▓███████▓▓███▓╬╬╬╬╬╬▓███████▓╬╬╬╬▓█" 
+echo "████████████████▓█▓╬╬╬╬╬▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬█" 
+echo "███▓▓▓▓▓▓▓╬╬▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█" 
+echo "████▓▓▓╬╬╬╬▓▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█" 
+echo "███▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█" 
+echo "█████▓▓▓▓▓▓▓▓█▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█" 
+echo "█████▓▓▓▓▓▓▓██▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██" 
+echo "█████▓▓▓▓▓████▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██" 
+echo "████▓█▓▓▓▓██▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██" 
+echo "████▓▓███▓▓▓▓▓▓▓██▓╬╬╬╬╬╬╬╬╬╬╬╬█▓╬▓╬╬▓██" 
+echo "█████▓███▓▓▓▓▓▓▓▓████▓▓╬╬╬╬╬╬╬█▓╬╬╬╬╬▓██" 
+echo "█████▓▓█▓███▓▓▓████╬▓█▓▓╬╬╬▓▓█▓╬╬╬╬╬╬███" 
+echo "██████▓██▓███████▓╬╬╬▓▓╬▓▓██▓╬╬╬╬╬╬╬▓███" 
+echo "███████▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬████" 
+echo "███████▓▓██▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓████" 
+echo "████████▓▓▓█████▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█████" 
+echo "█████████▓▓▓█▓▓▓▓▓███▓╬╬╬╬╬╬╬╬╬╬╬▓██████"
+echo "██████████▓▓▓█▓▓▓╬▓██╬╬╬╬╬╬╬╬╬╬╬▓███████" 
+echo "███████████▓▓█▓▓▓▓███▓╬╬╬╬╬╬╬╬╬▓████████"
+echo "██████████████▓▓▓███▓▓╬╬╬╬╬╬╬╬██████████"
+echo "███████████████▓▓▓██▓▓╬╬╬╬╬╬▓███████████"
+sleep 10s
+watch dapscoin-cli06 -rpcuser=DAPSMN06 -rpcpassword=DAPSMN060 -rpcport=53579 getinfo
