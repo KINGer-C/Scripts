3c543deffe1ede412b7e1485f527d38a7ca4f52c
KINGer-C <paulodtna12@gmail.com>
2019-11-08 05:05:25 -0300

    Create daps.sh

---
+++ daps.sh
@@ -0,0 +1,52 @@
+echo "#################################"
+echo "### DAPS MASTERNODE EASY WAY ####"
+echo "########## By KINGer-C ##########"
+echo "#################################"
+sleep 2s
+clear
+echo "░░░░░▄▄▀▀▀▀▀▀▀▀▀▄▄░░░░░"
+echo "░░░░█░░░░░░░░░░░░░█░░░░"
+echo "░░░█░░░░░░░░░░▄▄▄░░█░░░"
+echo "░░░█░░▄▄▄░░▄░░███░░█░░░"
+echo "░░░▄█░▄░░░▀▀▀░░░▄░█▄░░░"
+echo "░░░█░░▀█▀█▀█▀█▀█▀░░█░░░"
+echo "░░░▄██▄▄▀▀▀▀▀▀▀▄▄██▄░░░"
+echo "░▄█░█▀▀█▀▀▀█▀▀▀█▀▀█░█▄░"
+echo "▄▀░▄▄▀▄▄▀▀▀▄▀▀▀▄▄▀▄▄░▀▄"
+echo "█░░░░▀▄░█▄░░░▄█░▄▀░░░░█"
+echo "░▀▄▄░█░░█▄▄▄▄▄█░░█░▄▄▀░"
+echo "░░░▀██▄▄███████▄▄██▀░░░"
+echo "░░░████████▀████████░░░"
+echo "░░▄▄█▀▀▀▀█░░░█▀▀▀▀█▄▄░░"
+echo "░░▀▄▄▄▄▄▀▀░░░▀▀▄▄▄▄▄▀░"
+sleep 2s
+clear
+ls -al
+echo "WHAT DO YOU WANNA DO?"
+echo "0 - Install the IPV4 DAPSMN00"
+echo "1 - Install the IPV6 DAPSMN01"
+echo "2 - Install the IPV6 DAPSMN02"
+echo "3 - Install the IPV6 DAPSMN03"
+echo "4 - Install the IPV6 DAPSMN04"
+echo "5 - Install the IPV6 DAPSMN05"
+echo "6 - Install the IPV6 DAPSMN06"
+read choise
+if [ $choise = 0 ]; then
+  rm -rf dapsipv4.sh && wget https://raw.githubusercontent.com/KINGer-C/Scripts/master/dapsipv4.sh && chmod 777 dapsipv4.sh && bash dapsipv4.sh
+elfi [ $choise = 1]; then
+  rm -rf dapsipv61_DAPSMN01.sh && wget https://raw.githubusercontent.com/KINGer-C/Scripts/master/dapsipv61_DAPSMN01.sh && chmod 777 dapsipv61_DAPSMN01.sh && bash dapsipv61_DAPSMN01.sh
+elfi [ $choise = 2]; then
+  rm -rf dapsipv61_DAPSMN02.sh && wget https://raw.githubusercontent.com/KINGer-C/Scripts/master/dapsipv61_DAPSMN02.sh && chmod 777 dapsipv61_DAPSMN02.sh && bash dapsipv61_DAPSMN02.sh
+elfi [ $choise = 3]; then
+  rm -rf dapsipv61_DAPSMN03.sh && wget https://raw.githubusercontent.com/KINGer-C/Scripts/master/dapsipv61_DAPSMN03.sh && chmod 777 dapsipv61_DAPSMN03.sh && bash dapsipv61_DAPSMN03.sh
+elfi [ $choise = 4]; then
+  rm -rf dapsipv61_DAPSMN04.sh && wget https://raw.githubusercontent.com/KINGer-C/Scripts/master/dapsipv61_DAPSMN04.sh && chmod 777 dapsipv61_DAPSMN04.sh && bash dapsipv61_DAPSMN04.sh
+elfi [ $choise = 5]; then
+  rm -rf dapsipv61_DAPSMN05.sh && wget https://raw.githubusercontent.com/KINGer-C/Scripts/master/dapsipv61_DAPSMN05.sh && chmod 777 dapsipv61_DAPSMN05.sh && bash dapsipv61_DAPSMN05.sh
+elfi [ $choise = 5]; then
+  rm -rf dapsipv61_DAPSMN06.sh && wget https://raw.githubusercontent.com/KINGer-C/Scripts/master/dapsipv61_DAPSMN06.sh && chmod 777 dapsipv61_DAPSMN06.sh && bash dapsipv61_DAPSMN06.sh
+ else
+  echo" Command not found, please restart the instalation"
+fi
+
+
