0065a379a089ab860c09eada82789d76a5217393
KINGer-C <paulodtna12@gmail.com>
2019-11-08 04:40:03 -0300

    Update dapsipv4.sh

--- dapsipv4.sh
+++ dapsipv4.sh
@@ -23,6 +23,7 @@ clear
 echo "Do you wanna update and upgrade the system y/n?"
 read var
 if [ $var = y ]
+echo "Upgrading"
 sudo apt-get install unzip && sudo apt-get  update && sudo apt-get  upgrade -y
 fi
 echo " DONE!!!"
@@ -31,6 +32,7 @@ clear
 echo "Do you wanna to swap 4G Memory? y/n"
 read var1
 if [ $var1 = y ]
+echo "Swaping"
 sudo fallocate -l 4G /swapfile && chmod 600 /swapfile && mkswap /swapfile && swapon /swapfile && echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab && free -h
 fi
 echo " DONE!!!"
