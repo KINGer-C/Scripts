c6fc75df5ab9553effe8b9ed65f29747a21ac52b
KINGer-C <paulodtna12@gmail.com>
2019-11-20 16:50:02 -0300

    pac update

---
+++ pacup.sh
@@ -0,0 +1,14 @@
+echo " Stoping node ..."
+cd ~/PACGlobal && ./pacglobal-cli stop
+echo "Wallet successfully stoped"
+sleep 2;
+echo "Deleting files .."
+echo "Deleting pacglobal executables"
+rm -rf PACGlobal
+echo "Done!"
+echo "Downloading new wallet"
+wget https://github.com/PACGlobalOfficial/PAC/releases/download/035d4df02/pacglobal-035d4df02-lin64.tgz && chmod -x pacglobal-035d4df02-lin64.tgz 
+mkdir PACGlobal
+sudo tar -xvf pacglobal-035d4df02-win64.zip ~/PACGlobal
+cd ~/PACGlobal && ./paccoind
+echo "Masternode sucessfull updated!"
