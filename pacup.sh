e71ebb7660c32b27a0448decf2e08773d74f03cd
KINGer-C <paulodtna12@gmail.com>
2019-11-20 16:58:50 -0300

    Update pacup.sh

--- pacup.sh
+++ pacup.sh
@@ -5,11 +5,11 @@ echo "Wallet successfully stoped"
 sleep 2;
 echo "Deleting files .."
 echo "Deleting pacglobal executables"
-rm -rf PACGlobal
+rm -rf ~/PACGlobal
 echo "Done!"
 echo "Downloading new wallet"
 wget https://github.com/PACGlobalOfficial/PAC/releases/download/035d4df02/pacglobal-035d4df02-lin64.tgz && chmod -x pacglobal-035d4df02-lin64.tgz 
-mkdir PACGlobal
+mkdir ~/PACGlobal
 sudo tar -xvf pacglobal-035d4df02-lin64.tgz ~/PACGlobal
 ExecStart=/root/PACGlobal/pacglobald -daemon -pid=/root/.PACGlobal/pacglobal.pid \
           -conf=/root/.PACGlobal/pacglobal.conf -datadir=/root/.PACGlobal/
