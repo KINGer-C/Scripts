f556463ac4c2870450550bd1b17b9d8b92381892
KINGer-C <paulodtna12@gmail.com>
2019-11-20 17:16:45 -0300

    Update pacup.sh

--- pacup.sh
+++ pacup.sh
@@ -2,16 +2,17 @@ echo " Stoping node ..."
 ExecStop=-/root/PACGlobal/pacglobal-cli -conf=/root/.PACGlobal/pacglobal.conf \
           -datadir=/root/.PACGlobal/ stop
 echo "Wallet successfully stoped"
-sleep 2
+sleep 2s
 echo "Deleting files .."
 echo "Deleting pacglobal executables"
 rm -rf ~/PACGlobal
-sleep 10
+sleep 10s
 echo "Done!"
 echo "Downloading new wallet"
 mkdir ~/PACGlobal 
-sleep 3
+sleep 3s
 wget https://github.com/PACGlobalOfficial/PAC/releases/download/035d4df02/pacglobal-035d4df02-lin64.tgz && chmod -x pacglobal-035d4df02-lin64.tgz && tar -xvzf pacglobal-035d4df02-lin64.tgz
+sleep 5s
 mv -v ~/pacglobal-035d4df02-lin64/pacglobald ~/PACGlobal 
 mv -v ~/pacglobal-035d4df02-lin64/pacglobal-cli ~/PACGlobal 
 mv -v ~/pacglobal-035d4df02-lin64/pacglobal-tx ~/PACGlobal
@@ -19,7 +20,7 @@ mv -v ~/pacglobal-035d4df02-lin64/pacglobal-qt ~/PACGlobal
 sudo chmod +x ~PACGlobal/pacglobal*
 rm -rf pacglobal-035d4df02-lin64.tgz
 rm -rf pacglobal-035d4df02-lin64
-sleep 3
+sleep 3s
 ExecStart=/root/PACGlobal/pacglobald -daemon -pid=/root/.PACGlobal/pacglobal.pid \
           -conf=/root/.PACGlobal/pacglobal.conf -datadir=/root/.PACGlobal/
 echo "Masternode sucessfull updated!"
