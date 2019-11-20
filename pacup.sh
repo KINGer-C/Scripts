099f3c70848eb7ae92f6de78a3aea4d715ee08f2
KINGer-C <paulodtna12@gmail.com>
2019-11-20 17:15:41 -0300

    Update pacup.sh

--- pacup.sh
+++ pacup.sh
@@ -11,7 +11,15 @@ echo "Done!"
 echo "Downloading new wallet"
 mkdir ~/PACGlobal 
 sleep 3
-cd ~/PACGlobal && wget https://github.com/PACGlobalOfficial/PAC/releases/download/035d4df02/pacglobal-035d4df02-lin64.tgz && chmod -x pacglobal-035d4df02-lin64.tgz && tar -xvzf pacglobal-035d4df02-lin64.tgz
+wget https://github.com/PACGlobalOfficial/PAC/releases/download/035d4df02/pacglobal-035d4df02-lin64.tgz && chmod -x pacglobal-035d4df02-lin64.tgz && tar -xvzf pacglobal-035d4df02-lin64.tgz
+mv -v ~/pacglobal-035d4df02-lin64/pacglobald ~/PACGlobal 
+mv -v ~/pacglobal-035d4df02-lin64/pacglobal-cli ~/PACGlobal 
+mv -v ~/pacglobal-035d4df02-lin64/pacglobal-tx ~/PACGlobal
+mv -v ~/pacglobal-035d4df02-lin64/pacglobal-qt ~/PACGlobal
+sudo chmod +x ~PACGlobal/pacglobal*
+rm -rf pacglobal-035d4df02-lin64.tgz
+rm -rf pacglobal-035d4df02-lin64
+sleep 3
 ExecStart=/root/PACGlobal/pacglobald -daemon -pid=/root/.PACGlobal/pacglobal.pid \
           -conf=/root/.PACGlobal/pacglobal.conf -datadir=/root/.PACGlobal/
 echo "Masternode sucessfull updated!"
