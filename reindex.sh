1776d6bc067eda861cc5ce269d22a269f1a636f4
KINGer-C <paulodtna12@gmail.com>
2019-12-03 00:13:14 -0300

    Update reindex.sh

--- reindex.sh
+++ reindex.sh
@@ -17,20 +17,20 @@ clear
 if [ $choise = 'rx0' ]; then
   dapscoin-cli stop && sleep 10s && dapscoind -reindex && sleep 10s && watch dapscoin-cli getinfo
 elif [ $choise = 'rx1' ]; then
-  stop02 && sleep 10s && start01 -reindex && sleep 10s && watch getinfo01
+  dapscoin-cli01 -rpcuser=DAPSMN01 -rpcpassword=DAPSMN010 -rpcport=53574 stop && sleep 10s && dapscoind01 -datadir=/root/.dapscoin01 -wallet=wallet01.dat -reindex && sleep 10s && watch dapscoin-cli01 -rpcuser=DAPSMN01 -rpcpassword=DAPSMN010 -rpcport=53574 getinfo
 elif [ $choise = 'rx2' ]; then
-  stop01 && sleep 10s && start02 -reindex && sleep 10s && watch getinfo02
+  dapscoin-cli02 -rpcuser=DAPSMN02 -rpcpassword=DAPSMN020 -rpcport=53575 stop && sleep 10s && dapscoind02 -datadir=/root/.dapscoin02 -wallet=wallet02.dat -reindex && sleep 10s && watch dapscoin-cli02 -rpcuser=DAPSMN02 -rpcpassword=DAPSMN020 -rpcport=53575 getinfo
 elif [ $choise = 'rx3' ]; then
-  stop03 && sleep 10s && start03 -reindex && sleep 10s && watch getinfo03
+  dapscoin-cli03 -rpcuser=DAPSMN03 -rpcpassword=DAPSMN030 -rpcport=53576 stop && sleep 10s && dapscoind03 -datadir=/root/.dapscoin03 -wallet=wallet03.dat -reindex && sleep 10s && watch dapscoin-cli03 -rpcuser=DAPSMN03 -rpcpassword=DAPSMN030 -rpcport=53576 getinfo
 elif [ $choise = 'rx4' ]; then
-  stop04 && sleep 10s && start04 -reindex && sleep 10s && watch getinfo04
+  dapscoin-cli04 -rpcuser=DAPSMN04 -rpcpassword=DAPSMN040 -rpcport=53577 stop && sleep 10s && dapscoind04 -datadir=/root/.dapscoin04 -wallet=wallet04.dat -reindex && sleep 10s && watch dapscoin-cli04 -rpcuser=DAPSMN04 -rpcpassword=DAPSMN040 -rpcport=53577 getinfo
 elif [ $choise = 'rx5' ]; then
-  stop05 && sleep 10s && start05 -reindex && sleep 10s && watch getinfo05
+  dapscoin-cli05 -rpcuser=DAPSMN05 -rpcpassword=DAPSMN050 -rpcport=53578 stop && sleep 10s && dapscoind05 -datadir=/root/.dapscoin05 -wallet=wallet05.dat -reindex && sleep 10s && watch dapscoin-cli05 -rpcuser=DAPSMN05 -rpcpassword=DAPSMN050 -rpcport=53578 getinfo
 elif [ $choise = 'rx6' ]; then
-  stop06 && sleep 10s && start06 -reindex && sleep 10s && watch getinfo06
+  dapscoin-cli06 -rpcuser=DAPSMN06 -rpcpassword=DAPSMN060 -rpcport=53579 stop && sleep 10s && dapscoind06 -datadir=/root/.dapscoin06 -wallet=wallet06.dat -reindex && sleep 10s && watch dapscoin-cli06 -rpcuser=DAPSMN06 -rpcpassword=DAPSMN060 -rpcport=53579 getinfo
 elif [ $choise = 'e' ]; then
-   rm -rf reindex.sh && break
+  rm -rf reindex.sh && break
 else 
-  echo "Command not found, Please try Again !" && sleep 3s
+  echo "Command not found, Please try Again !" && sleep 3s && clear
 fi
 done
