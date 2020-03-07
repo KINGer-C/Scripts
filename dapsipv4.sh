670d5c6d1909f108d9059fd8e18d5c99312a7932
KINGer-C <paulodtna12@gmail.com>
2020-03-07 14:31:51 -0300

    Update dapsipv4.sh

--- dapsipv4.sh
+++ dapsipv4.sh
@@ -22,11 +22,11 @@ echo "░░▀▄▄▄▄▄▀▀░░░▀▀▄▄▄▄▄▀░"
 sleep 3s
 clear
 echo "Downloading latest build..."
-wget -N https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.4/master_linux-v1.0.4.6.zip
+wget -N https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.5.8/dapscoin-v1.0.5.8-linux.zip
 echo "Installing unzip..."
 sudo apt-get install unzip -y
 echo "Unzipping latest zip..."
-sudo unzip -jo master_linux-v1.0.4.6.zip -d /usr/local/bin
+sudo unzip -jo dapscoin-v1.0.5.8-linux.zip -d /usr/local/bin
 echo "Giving permissions...."
 sudo chmod +x /usr/local/bin/daps*
 echo "Removing old files"
@@ -48,22 +48,10 @@ echo "rpcallowip=127.0.0.1" >> $CONF_DIR/$CONF_FILE
 echo "server=1" >> $CONF_DIR/$CONF_FILE
 echo "daemon=1" >> $CONF_DIR/$CONF_FILE
 echo "logtimestamps=1" >> $CONF_DIR/$CONF_FILE
-echo "maxconnections=32" >> $CONF_DIR/$CONF_FILE
+echo "maxconnections=16" >> $CONF_DIR/$CONF_FILE
 echo "masternode=1" >> $CONF_DIR/$CONF_FILE
 echo "masternodeaddr=$IP:$PORT" >> $CONF_DIR/$CONF_FILE
 echo "masternodeprivkey=$PRIVKEY" >> $CONF_DIR/$CONF_FILE
-echo "addnode=95.216.252.153:34536" >> $CONF_DIR/$CONF_FILE
-echo "addnode=95.216.84.192:37912" >> $CONF_DIR/$CONF_FILE
-echo "addnode=95.217.98.226:40586" >> $CONF_DIR/$CONF_FILE
-echo "addnode=95.217.70.158:50228" >> $CONF_DIR/$CONF_FILE
-echo "addnode=95.217.70.41:38154" >> $CONF_DIR/$CONF_FILE
-echo "addnode=95.216.253.172:48176" >> $CONF_DIR/$CONF_FILE
-echo "addnode=113.234.221.75:52758" >> $CONF_DIR/$CONF_FILE
-echo "addnode=220.214.11.115:64211" >> $CONF_DIR/$CONF_FILE
-echo "addnode=77.161.105.40:55110" >> $CONF_DIR/$CONF_FILE
-echo "addnode=89.40.9.159:52800" >> $CONF_DIR/$CONF_FILE
-echo "addnode=86.157.160.160:53572" >> $CONF_DIR/$CONF_FILE
-echo "addnode=95.216.94.80:39740" >> $CONF_DIR/$CONF_FILE
 
 sudo apt-get install fail2ban -y
 sudo ufw allow ssh
