a1949605cb4d9190f5fa498491c6d81b8910c064
KINGer-C <paulodtna12@gmail.com>
2020-04-25 14:58:25 -0300

    Update paw.sh

--- paw.sh
+++ paw.sh
@@ -28,14 +28,16 @@ echo "##########################################################################
 read choise
 clear
 echo "Downloading latest build..."
+
 if [ $choise = '16' ]; then
-  wget -N https://github.com/PawcoinNew/Pawcoin/releases/download/1.0/Pawcoinv1.0-Ubuntu16.tar.gz && sudo tar xvzf Pawcoinv1.0-Ubuntu16.tar.gz -C /usr/local/bin && sudo chmod +x /usr/local/bin/pawcoin*
+  wget https://github.com/PawcoinNew/Pawcoin/releases/download/1.0/Pawcoinv1.0-Ubuntu16.tar.gz && sudo tar xvzf Pawcoinv1.0-Ubuntu16.tar.gz -C /usr/local/bin && sudo chmod +x /usr/local/bin/pawcoin*
 elif [ $choise = '18' ]; then
-  wget -N https://github.com/PawcoinNew/Pawcoin/releases/download/1.0/Pawcoinv1.0.Ubuntu18.tar.gz && sudo tar xvzf Pawcoinv1.0.Ubuntu18.tar.gz -C /usr/local/bin && sudo chmod +x /usr/local/bin/pawcoin*
+  wget https://github.com/PawcoinNew/Pawcoin/releases/download/1.0/Pawcoinv1.0.Ubuntu18.tar.gz && sudo tar xvzf Pawcoinv1.0.Ubuntu18.tar.gz -C /usr/local/bin && sudo chmod +x /usr/local/bin/pawcoin*
 else
   echo "That is not a valid option, Please try again" 
-  exit
-  fi
+  exit -1
+fi
+
 echo "Giving permissions...."
 echo "Removing old files"
 rm -rf Pawcoinv1.0-Ubuntu16.tar.gz Pawcoinv1.0-Ubuntu18.tar.gz
