6c4f2a235e26f9752ab1cae6206c629ee394f9db
KINGer-C <paulodtna12@gmail.com>
2021-02-18 21:18:08 -0300

    Update daps.sh

--- daps.sh
+++ daps.sh
@@ -1,3 +1,20 @@
+echo "░░░░░▄▄▀▀▀▀▀▀▀▀▀▄▄░░░░░"
+echo "░░░░█░░░░░░░░░░░░░█░░░░"
+echo "░░░█░░░░░░░░░░▄▄▄░░█░░░"
+echo "░░░█░░▄▄▄░░▄░░███░░█░░░"
+echo "░░░▄█░▄░░░▀▀▀░░░▄░█▄░░░"
+echo "░░░█░░▀█▀█▀█▀█▀█▀░░█░░░"
+echo "░░░▄██▄▄▀▀▀▀▀▀▀▄▄██▄░░░"
+echo "░▄█░█▀▀█▀▀▀█▀▀▀█▀▀█░█▄░"
+echo "▄▀░▄▄▀▄▄▀▀▀▄▀▀▀▄▄▀▄▄░▀▄"
+echo "█░░░░▀▄░█▄░░░▄█░▄▀░░░░█"
+echo "░▀▄▄░█░░█▄▄▄▄▄█░░█░▄▄▀░"
+echo "░░░▀██▄▄███████▄▄██▀░░░"
+echo "░░░████████▀████████░░░"
+echo "░░▄▄█▀▀▀▀█░░░█▀▀▀▀█▄▄░░"
+echo "░░▀▄▄▄▄▄▀▀░░░▀▀▄▄▄▄▄▀░"
+sleep 3s
+clear
 clear
 echo "#################################"
 echo "### DAPS MASTERNODE EASY WAY ####"
@@ -5,6 +22,33 @@ echo "########## By KINGer-C ##########"
 echo "#################################"
 sleep 2s
 clear
+echo "####### DAPS - PRIVACITY MATTERS #######"
+echo "███████▓█████▓▓╬╬╬╬╬╬╬╬▓███▓╬╬╬╬╬╬╬▓╬╬▓█"
+echo "████▓▓▓▓╬╬▓█████╬╬╬╬╬╬███▓╬╬╬╬╬╬╬╬╬╬╬╬╬█"
+echo "███▓▓▓▓╬╬╬╬╬╬▓██╬╬╬╬╬╬▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█" 
+echo "████▓▓▓╬╬╬╬╬╬╬▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█" 
+echo "███▓█▓███████▓▓███▓╬╬╬╬╬╬▓███████▓╬╬╬╬▓█" 
+echo "████████████████▓█▓╬╬╬╬╬▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬█" 
+echo "███▓▓▓▓▓▓▓╬╬▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█" 
+echo "████▓▓▓╬╬╬╬▓▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█" 
+echo "███▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█" 
+echo "█████▓▓▓▓▓▓▓▓█▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█" 
+echo "█████▓▓▓▓▓▓▓██▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██" 
+echo "█████▓▓▓▓▓████▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██" 
+echo "████▓█▓▓▓▓██▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██" 
+echo "████▓▓███▓▓▓▓▓▓▓██▓╬╬╬╬╬╬╬╬╬╬╬╬█▓╬▓╬╬▓██" 
+echo "█████▓███▓▓▓▓▓▓▓▓████▓▓╬╬╬╬╬╬╬█▓╬╬╬╬╬▓██" 
+echo "█████▓▓█▓███▓▓▓████╬▓█▓▓╬╬╬▓▓█▓╬╬╬╬╬╬███" 
+echo "██████▓██▓███████▓╬╬╬▓▓╬▓▓██▓╬╬╬╬╬╬╬▓███" 
+echo "███████▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬████" 
+echo "███████▓▓██▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓████" 
+echo "████████▓▓▓█████▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█████" 
+echo "█████████▓▓▓█▓▓▓▓▓███▓╬╬╬╬╬╬╬╬╬╬╬▓██████"
+echo "██████████▓▓▓█▓▓▓╬▓██╬╬╬╬╬╬╬╬╬╬╬▓███████" 
+echo "███████████▓▓█▓▓▓▓███▓╬╬╬╬╬╬╬╬╬▓████████"
+echo "██████████████▓▓▓███▓▓╬╬╬╬╬╬╬╬██████████"
+echo "███████████████▓▓▓██▓▓╬╬╬╬╬╬▓███████████"
+sleep 10s
 sleep 2s
 clear
 while :
@@ -28,7 +72,56 @@ echo "##########################################################################
 read choise
 clear
 if [ $choise = 0 ]; then
-  rm -rf dapsipv4.sh && wget https://raw.githubusercontent.com/KINGer-C/Scripts/master/dapsipv4.sh && chmod 777 dapsipv4.sh && bash dapsipv4.sh
+     clear
+    echo "#############################"
+    echo "### DAPS IVP4 By KINGer-C ###"
+    echo "#############################"
+    sleep 2s
+    clear
+
+    echo "Downloading latest build..."
+    wget -N https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.5.8/dapscoin-v1.0.5.8-linux.zip
+    echo "Installing unzip..."
+    sudo apt-get install unzip -y
+    echo "Unzipping latest zip..."
+    sudo unzip -jo dapscoin-v1.0.5.8-linux.zip -d /usr/local/bin
+    echo "Giving permissions...."
+    sudo chmod +x /usr/local/bin/daps*
+    echo "Removing old files"
+    rm -rf master_linux-v1.0.4.6.zip
+    rm -rf ~/.dapscoin/
+    echo "Enter your IPV4 for the DAPSMN00"
+    read IP
+    echo "Enter your masternode private key for node DAPSMN00"
+    read PRIVKEY
+    
+    CONF_DIR=~/.dapscoin/
+    CONF_FILE=dapscoin.conf
+    PORT=53572
+
+    mkdir -p $CONF_DIR
+    echo "rpcuser=DAPSMN00" >> $CONF_DIR/$CONF_FILE
+    echo "rpcpassword=DAPSMN000" >> $CONF_DIR/$CONF_FILE
+    echo "rpcallowip=127.0.0.1" >> $CONF_DIR/$CONF_FILE
+    echo "server=1" >> $CONF_DIR/$CONF_FILE
+    echo "daemon=1" >> $CONF_DIR/$CONF_FILE
+    echo "logtimestamps=1" >> $CONF_DIR/$CONF_FILE
+    echo "maxconnections=16" >> $CONF_DIR/$CONF_FILE
+    echo "masternode=1" >> $CONF_DIR/$CONF_FILE
+    echo "masternodeaddr=$IP:$PORT" >> $CONF_DIR/$CONF_FILE
+    echo "masternodeprivkey=$PRIVKEY" >> $CONF_DIR/$CONF_FILE
+
+    sudo apt-get install fail2ban -y
+    sudo ufw allow ssh
+    sudo ufw allow 53572
+    sudo ufw allow 53573
+    sudo ufw enable
+    sleep 2s
+    clear
+    dapscoind -daemon
+    echo " FINISH :)"
+
+watch dapscoin-cli getinfo
 elif [ $choise = '1' ]; then
   rm -rf dapsipv61_DAPSMN01.sh && wget https://raw.githubusercontent.com/KINGer-C/Scripts/master/dapsipv61_DAPSMN01.sh && chmod 777 dapsipv61_DAPSMN01.sh && bash dapsipv61_DAPSMN01.sh
 elif [ $choise = '2' ]; then
